7bdfd5d8587804c1a5e99fc221810f17e57aba30
Peter Curtis <peterrobertcurtis@gmail.com>
2017-01-19 14:07:42 +0000

    Fixed S3 generic/method consistency

--- R/plot_SSM.R
+++ R/plot_SSM.R
@@ -19,13 +19,13 @@
 #' interval for the metamodel error estimating Gaussian process if this has been
 #' computed for the SSM object.
 #'
-#' @param ssm An SSM object.
+#' @param x An SSM object.
+#' @param ... (optional) arguments to pass to the \code{plot} function.
 #' @param grid (optional) A number. This specifies the resolution of the plot,
 #'   \emph{i.e.} how many model evaluations are used to construct the curve.
 #' @param yrange (optional) Character. Only "full" will have an effect.
 #' @param GP (optional) Logical. For single variable data, the credible interval
 #'   of the metamodel error estimator will be plotted if TRUE.
-#' @param ... (optional) arguments to pass to the \code{plot} function.
 #' @export
 #' @examples
 #' # A single variable example
@@ -40,8 +40,8 @@
 #' Y <- attitude[ , 1]
 #' s <- fit.ssm(X, Y)
 #' plot(s)
-plot.SSM <- function(ssm, grid = 200, yrange = "full", GP = TRUE, ...){
-  # plots main effects for each variable over [-1,1]
+plot.SSM <- function(x, ..., grid = 200, yrange = "full", GP = TRUE){
+  # plots main effects for each variable over [-1,1] when x is an SSM object
   # if ssm is univariate, will plot the ssm with GP metamodel error if computed
   # arguments:
   #     grid:    specifies the resolution of the plot.
@@ -53,21 +53,21 @@ plot.SSM <- function(ssm, grid = 200, yrange = "full", GP = TRUE, ...){
   #     ...:     arguments passed on to plot (overrides default behaviour).
   #              These do not effect the plotting of design points though.
   arguments <- list(...)
-  if (yrange[1] == "response") yrange <- range(ssm@response)
+  if (yrange[1] == "response") yrange <- range(x@response)
   if(is.null(arguments$xlim))
     arguments$xlim <- c(-1,1)
   arguments$type <- "l"
 
-  if (ssm@dimension == 1){
-    x <- seq(arguments$xlim[1], arguments$xlim[2], length.out = grid)
+  if (x@dimension == 1){
+    z <- seq(arguments$xlim[1], arguments$xlim[2], length.out = grid)
     y <- c()
-    for (i in 1:grid) y[3 * i - 2:0] <- as.numeric(predict(ssm, x[i]))
+    for (i in 1:grid) y[3 * i - 2:0] <- as.numeric(predict(x, z[i]))
     y <- matrix(y, ncol=3, byrow=TRUE)
     if (yrange[1] == "full")
       yrange <- range(y)
     if(is.null(arguments$ylim))
       arguments$ylim <- yrange
-    arguments$x <- cbind(x, y[,2])
+    arguments$x <- cbind(z, y[,2])
     do.call(plot, arguments)
     #    plot(
     #         x, y[,1],
@@ -82,37 +82,37 @@ plot.SSM <- function(ssm, grid = 200, yrange = "full", GP = TRUE, ...){
     #    points(x, y[,3], type = "l", lty = "dashed")
 
     ## The following uses grey shading for CI
-    polygon(c(x, rev(x)), c(y[,1], rev(y[,3])), border = NA, col = "lightgray")
-    lines(x, y[,2])
+    polygon(c(z, rev(z)), c(y[,1], rev(y[,3])), border = NA, col = "lightgray")
+    lines(z, y[,2])
 
-    points(ssm@design[1:ssm@design_size,], ssm@response, pch=16)
+    points(x@design[1:x@design_size,], x@response, pch=16)
   } else {
 
-    if (length(ssm@main_sobol) < 1)
-      ssm <- update.sensitivity(ssm)
+    if (length(x@main_sobol) < 1)
+      x <- update.sensitivity(x)
 
-    x <- seq(arguments$xlim[1], arguments$xlim[2], length.out = grid)
-    xx <- matrix(rep(x, ssm@dimension), ncol=ssm@dimension)
-    X <- construct.dmm(ssm@basis, xx, ssm@P)
-    main_theta <- matrix(rep(ssm@theta, ssm@dimension),
-                         ncol = ssm@dimension) * ssm@main_ind
+    z <- seq(arguments$xlim[1], arguments$xlim[2], length.out = grid)
+    zz <- matrix(rep(z, x@dimension), ncol=x@dimension)
+    X <- construct.dmm(x@basis, zz, x@P)
+    main_theta <- matrix(rep(x@theta, x@dimension),
+                         ncol = x@dimension) * x@main_ind
     res <- X %*% main_theta
     par(
       mfrow = c(
-        ceiling(sqrt(ssm@dimension)),
-        ceiling(ssm@dimension / ceiling(sqrt(ssm@dimension)))
+        ceiling(sqrt(x@dimension)),
+        ceiling(x@dimension / ceiling(sqrt(x@dimension)))
       ),
       mar = c(1,2,1,1)
     )
-    for (i in 1:ssm@dimension){
-      arguments$x <- cbind(x, res[ , i] + ssm@theta[1])
+    for (i in 1:x@dimension){
+      arguments$x <- cbind(z, res[ , i] + x@theta[1])
       if (yrange == "full"){
-        arguments$ylim <- range(res[ , i] + ssm@theta[1])
+        arguments$ylim <- range(res[ , i] + x@theta[1])
       } else {
-        arguments$ylim <- range(ssm@response)
+        arguments$ylim <- range(x@response)
       }
       do.call(plot, arguments)
-      points(ssm@design[ , i], ssm@response, pch = 16)
+      points(x@design[ , i], x@response, pch = 16)
     }
   }
 }
@@ -124,10 +124,11 @@ plot.SSM <- function(ssm, grid = 200, yrange = "full", GP = TRUE, ...){
 #' SSM has a metamodel error estimate then a \eqn{(1 - \alpha)} credible
 #' interval is also output.
 #'
-#' @param ssm An SSM object.
+#' @param object An SSM object.
 #' @param x A \eqn{d} length vector identifying the prediction point.
 #' @param alpha (optional) A number in \eqn{[0, 1]} for the \eqn{(1 - \alpha)}
 #'   metamodel error estimate credible interval. Set to \code{0.05} by default.
+#' @param ... further arguments passed to or from other methods.
 #' @return Either a number if the SSM has no metamodel error estimating Gaussian
 #' process, or three numbers giving the model prediction (\code{$model}), and
 #' the lower and upper bounds of the credible interval (\code{$lower} and
@@ -144,36 +145,36 @@ plot.SSM <- function(ssm, grid = 200, yrange = "full", GP = TRUE, ...){
 #' # with metamodel error estimating GP.
 #' s <- fit.ssm(X, Y, GP = TRUE)
 #' predict(s, rep(1,6))
-predict.SSM <- function (ssm, x, alpha=0.05){
+predict.SSM <- function (object, x, alpha=0.05, ...){
   # uses the ssm to predict an observation at point x
   # arguments:
-  #   ssm:      the "ssm" fitted model.
+  #   object:      the "ssm" fitted model.
   #   x:        the prediction location
   #   alpha:    Set the alpha value for the prediction interval if a metamodel
   #             GP has been computed.
 
-  if(ssm@fail == TRUE) stop("The SSM must be fit to data before it can be used
+  if(object@fail == TRUE) stop("The SSM must be fit to data before it can be used
                             for prediction.\n")
 
-  if (ssm@dimension == 1){
-    X <- (rep(x, ssm@basis_size) ^ t(ssm@basis)) %*% ssm@P
+  if (object@dimension == 1){
+    X <- (rep(x, object@basis_size) ^ t(object@basis)) %*% object@P
   } else {
-    X <- matrix (rep (x, ssm@basis_size), nrow = ssm@dimension)
-    X <- (X ^ t(ssm@basis)) %*% ssm@P
+    X <- matrix (rep (x, object@basis_size), nrow = object@dimension)
+    X <- (X ^ t(object@basis)) %*% object@P
     X <- apply (X, 2, prod)
   }
 
-  fit   <- sum(X %*% ssm@theta)
+  fit   <- sum(X %*% object@theta)
   bound <- 0
 
-  if (length(ssm@r)>0){
-    s <- ssm
-    s@design <- rbind(x, ssm@design)
-    new.dist <- new.distance(ssm = s, type = ssm@distance_type)[1, -1]
+  if (length(object@r)>0){
+    s <- object
+    s@design <- rbind(x, object@design)
+    new.dist <- new.distance(s, type = object@distance_type)[1, -1]
     if(sum(new.dist == 0) > 0)
       return(list(lower = fit, model = fit, upper = fit))
-    new.cov  <- compute.covariance.from.distance(new.dist, ssm@r, ssm@type, ssm)
-    V <- ssm@sigma * (1 - sum(diag(solve(ssm@covariance,
+    new.cov  <- compute.covariance.from.distance(new.dist, object@r, object@type, object)
+    V <- object@sigma * (1 - sum(diag(solve(object@covariance,
                                          new.cov %*% t(new.cov)))))
     if(V < 0) V <- 0
     if(V > 1) V <- 1
